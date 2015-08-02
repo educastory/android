c539fcff865de007ad314b53774fdc950105dfec
mizukami <b14723bb@aitech.ac.jp>
2015-08-02 14:15:32 +0900

    add function button

--- app/src/main/java/educa/educastory/MainActivity.java
+++ app/src/main/java/educa/educastory/MainActivity.java
@@ -12,6 +12,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -19,11 +22,29 @@ public class MainActivity extends ActionBarActivity  implements TextToSpeech.OnI
 
     protected static TextToSpeech tts;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button answerBtn1 = (Button) findViewById(R.id.AnswerButton1);
+        answerBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "AnswerButton1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button answerBtn2 = (Button) findViewById(R.id.AnswerButton2);
+        answerBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "AnswerButton2", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }//end onCreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

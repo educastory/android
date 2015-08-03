ae23b19459cb041c7e6484b5c77ccb49608e79d5
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-08-04 01:22:12 +0900

    ダイアログの背景色を濃いグレーに指定した

---
+++ app/src/main/java/educa/educastory/TitleActivity.java
@@ -0,0 +1,13 @@
+package educa.educastory;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class TitleActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
